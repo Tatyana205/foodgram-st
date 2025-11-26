8b88dfcecb598e8a776a6e3552654d03ccc507d1
Татьяна <tatyana.v.skornyakova@yandex.ru>
2025-11-26 18:16:11 +0300

    apps, settings, requirements

---
+++ backend/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
